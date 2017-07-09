be786b4178f8bcec55a98c279ba938ee83d1ec72
rficu <rficu@airmail.cc>
2017-07-09 12:17:43 +0300

    Add assembly level io functions

---
+++ kernel/include/kernel/io.h
@@ -0,0 +1,12 @@
+#ifndef __IO_H__
+#define __IO_H__
+
+void outb(uint32_t addr, uint8_t v);  /* byte */
+void outw(uint32_t addr, uint16_t v); /* word */
+void outl(uint32_t addr, uint32_t v); /* dword */
+
+uint8_t inb(uint32_t addr);
+uint16_t inw(uint32_t addr);
+uint32_t inl(uint32_t addr);
+
+#endif /* end of include guard: __IO_H__ */
