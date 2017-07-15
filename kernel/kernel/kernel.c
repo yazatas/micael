a250dcd36065f31ef37cd085bcf8b594b2ecb53f
rficu <rficu@airmail.cc>
2017-07-15 16:53:03 +0300

    Add global descriptor table

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/gdt.h>
 
 void kernel_main(void) {
+	gdt_init();
 	term_init();
 
-	for (char c = 65; c <= 90; ++c) {
-		putchar(c);
+	puts("haloust");
 
-		if (c < 90)
-			putchar('\n');
-	}
+	/* for (char c = 65; c <= 90; ++c) { */
+	/* 	putchar(c); */
+
+	/* 	if (c < 90) */
+	/* 		putchar('\n'); */
+	/* } */
 }
