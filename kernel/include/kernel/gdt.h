2515b98f7a124ae19812d29b6298526cb47b7e70
rficu <rficu@airmail.cc>
2017-07-22 12:36:19 +0300

    Fix gdt table size bug

--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -18,10 +18,11 @@ struct gdt_desc_t {
     uint8_t  base24_31;
 } __attribute__((packed));
 
-#define GDT_BASE 0x00000800
-#define GDT_SIZE (sizeof(struct gdt_desc_t))
+#define GDT_BASE       0x00000800
+#define GDT_TABLE_SIZE 3
+#define GDT_ENTRY_SIZE (sizeof(struct gdt_desc_t))
 
-extern struct gdt_desc_t gmts[GDT_SIZE];
+extern struct gdt_desc_t gmts[GDT_TABLE_SIZE];
 extern struct gdt_reg_t gdt_reg;
 
 void gdt_init(void);
