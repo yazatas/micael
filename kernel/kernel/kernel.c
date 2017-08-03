37f80e710ce1086b9ad49d7168e476faf5149b2e
rficu <rficu@airmail.cc>
2017-08-03 09:15:07 +0300

    Enable interrupts

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
+#include <limits.h>
+#include <stdint.h>
+
 #include <kernel/tty.h>
 #include <kernel/gdt.h>
+#include <kernel/idt.h>
 
-void kernel_main(void) {
+void kernel_main(void)
+{
 	gdt_init();
+	idt_init();
 	term_init();
 
-	puts("haloust");
-
-	/* for (char c = 65; c <= 90; ++c) { */
-	/* 	putchar(c); */
-
-	/* 	if (c < 90) */
-	/* 		putchar('\n'); */
-	/* } */
+	int v = 10 / 0;
 }
