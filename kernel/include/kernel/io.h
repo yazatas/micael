409df48c4222805679f2a0e62c7617cf56331313
rficu <rficu@airmail.cc>
2017-08-05 21:09:19 +0300

    inline low-level io functions and add io_wait

--- kernel/include/kernel/io.h
+++ kernel/include/kernel/io.h
@@ -1,12 +1,12 @@
 #ifndef __IO_H__
 #define __IO_H__
 
-void outb(uint32_t addr, uint8_t v);  /* byte */
-void outw(uint32_t addr, uint16_t v); /* word */
-void outl(uint32_t addr, uint32_t v); /* dword */
+inline void outb(uint32_t addr, uint8_t v);  /* byte */
+inline void outw(uint32_t addr, uint16_t v); /* word */
+inline void outl(uint32_t addr, uint32_t v); /* dword */
 
-uint8_t inb(uint32_t addr);
-uint16_t inw(uint32_t addr);
-uint32_t inl(uint32_t addr);
+inline uint8_t inb(uint32_t addr);
+inline uint16_t inw(uint32_t addr);
+inline uint32_t inl(uint32_t addr);
 
 #endif /* end of include guard: __IO_H__ */
