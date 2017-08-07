2a06e14be97ea5cea5d440ba56273a8d61abd733
rficu <rficu@airmail.cc>
2017-08-07 13:41:28 +0300

    remove inline defs from header

--- kernel/include/kernel/io.h
+++ kernel/include/kernel/io.h
@@ -3,12 +3,12 @@
 
 #include <stdint.h>
 
-inline void outb(uint32_t addr, uint8_t v);  /* byte */
-inline void outw(uint32_t addr, uint16_t v); /* word */
-inline void outl(uint32_t addr, uint32_t v); /* dword */
+void outb(uint32_t addr, uint8_t v);  /* byte */
+void outw(uint32_t addr, uint16_t v); /* word */
+void outl(uint32_t addr, uint32_t v); /* dword */
 
-inline uint8_t inb(uint32_t addr);
-inline uint16_t inw(uint32_t addr);
-inline uint32_t inl(uint32_t addr);
+uint8_t inb(uint32_t addr);
+uint16_t inw(uint32_t addr);
+uint32_t inl(uint32_t addr);
 
 #endif /* end of include guard: __IO_H__ */
