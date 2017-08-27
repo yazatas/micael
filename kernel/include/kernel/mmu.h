4fa40c984483884314ae567c4c41d2d435524fb2
rficu <rficu@airmail.cc>
2017-08-27 12:33:36 +0300

    Enable demand paging Add page fault handler

--- kernel/include/kernel/mmu.h
+++ kernel/include/kernel/mmu.h
@@ -2,16 +2,22 @@
 #define __MMU_H__
 
 #include <stdint.h>
+#include <stddef.h>
 
 #define PF_SIZE             0x1000
+#define PAGE_SIZE           0x1000
+#define PE_SIZE             0x80000
 #define KALLOC_NO_MEM_ERROR 0xffffffff
 
 typedef uint32_t pageframe_t;
 
+/* general stuff */
 void mmu_init(void);
+void pf_handler(uint32_t error);
 void *get_physaddr(void *virtaddr);
 
-void kfree_frame(pageframe_t frame);
+/* allocation/deallocation  */
 pageframe_t kalloc_frame(void);
+void kfree_frame(pageframe_t frame);
 
 #endif /* end of include guard: __MMU_H__ */
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -13,17 +13,34 @@
 #include <kernel/kpanic.h>
 #include <kernel/mmu.h>
 
+extern uint32_t __kernel_virtual_start,  __kernel_virtual_end;
+extern uint32_t __kernel_physical_start, __kernel_physical_end;
+extern uint32_t boot_page_dir; 
 void kmain(void)
 {
     tty_init_default();
-	gdt_init();
-	idt_init();
-	irq_init();
+
+	kprint("kvirtual kphysical start: 0x%08x 0x%08x\n"
+		   "kvirtual kphysical end:   0x%08x 0x%08x\n",
+		   &__kernel_virtual_start, &__kernel_physical_start,
+		   &__kernel_virtual_end,   &__kernel_physical_end);
+	kprint("kpage dir start addr:     0x%08x\n", &boot_page_dir);
+
+	gdt_init(); idt_init();
+	irq_init(); timer_install();
+	kb_install();
 	asm ("sti"); /* enable interrupts */
 
-	timer_install();
-	kb_install();
+	mmu_init();
 
-	kprint("haloust");
+	
+	uint32_t *t = (uint32_t*)0xb0175012;
+	*t = 1337;
+	kprint("%u\n", *t);
+
+	uint32_t *v = (uint32_t*)0x59015099;
+	*v = 123;
+	kprint("%u\n", *v);
+
 	for (;;);
 }
