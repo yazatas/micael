1485b3cb3ef1c0d13fabde616186d008b15e8126
rficu <rficu@airmail.cc>
2017-09-15 10:20:06 +0300

    Finish kernel heap (krealloc and morecore)

--- kernel/kernel/kheap.c
+++ kernel/kernel/kheap.c
@@ -1,5 +1,7 @@
 #include <kernel/mmu.h>
 #include <kernel/kprint.h>
+#include <kernel/kheap.h>
+#include <kernel/kpanic.h>
 #include <string.h>
 
 typedef struct meta {
@@ -15,30 +17,57 @@ typedef struct meta {
 #define UNMARK_FREE(x) (x->flags &= ~0x1)
 #define GET_BASE(x)    ((meta_t*)x - 1)
 
-static meta_t *kernel_base   = NULL;
+static meta_t   *kernel_base = NULL;
 static uint32_t *HEAP_START  = (uint32_t*)0xd0000000;
+static uint32_t *HEAP_BREAK  = (uint32_t*)0xd0000000;
 
+/* TODO: this never returns NULL, map_page status code when??? */
 static meta_t *morecore(size_t size)
 {
-	/* TODO: how to know where the current heap break is?? */
-	/* TODO: create separate physical address container for kheap?? */
+	size_t pgcount = size / 0x1000 + 1;
+	meta_t *tmp = (meta_t*)HEAP_BREAK;
 
-	/* TODO: i don't need to keep track of physical pages (that's done by kalloc_frame)
-	 * but instead i need to know where the current break is (what info do i need to 
-	 * acquire that knowled??) */
+	for (size_t i = 0; i < pgcount; ++i) {
+		map_page((void*)kalloc_frame(), HEAP_BREAK, P_PRESENT | P_READWRITE);
+		HEAP_BREAK = (uint32_t*)((uint8_t*)HEAP_BREAK + 0x1000);
+	}
+	flush_tlb();
+
+	/* place the new block at the beginning of block list */
+	tmp->size = pgcount * 0x1000 - META_SIZE;
+	kernel_base->prev = tmp;
+	tmp->next = kernel_base;
+	tmp->prev = NULL;
+	kernel_base = tmp;
+	MARK_FREE(tmp);
+
+	return tmp;
 }
 
-static meta_t *split_block(meta_t *b, size_t split)
+/* append b2 to b1 (b2 becomes invalid) */
+static inline void merge_blocks(meta_t *b1, meta_t *b2)
 {
-	if (b->size <= split || b->size - split - META_SIZE <= 0)
-		return NULL;
+	b1->size += b2->size + META_SIZE;
+	b1->next = b2->next;
+	if (b2->next)
+		b2->next->prev = b1;
+}
+
+static void split_block(meta_t *b, size_t split)
+{
+	if (b->size <= split + META_SIZE)
+		return;
 
 	meta_t *tmp = (meta_t*)((uint8_t*)b + split + META_SIZE);
 	tmp->size = b->size - split - META_SIZE;
 	b->size = split;
 	MARK_FREE(tmp);
 
-	return tmp;
+	tmp->next = b->next;
+	if (b->next)
+		b->next->prev = tmp;
+	tmp->prev = b;
+	b->next = tmp;
 }
 
 static meta_t *find_free_block(size_t size)
@@ -51,27 +80,24 @@ static meta_t *find_free_block(size_t size)
 
 	if (b != NULL) {
 		UNMARK_FREE(b);
-
-		if ((tmp = split_block(b, size)) != NULL) {
-			tmp->next = b->next;
-			if (b->next)
-				b->next->prev = tmp;
-			tmp->prev = b;
-			b->next = tmp;
-		}
+		split_block(b, size);
 	}
 	return b;
 }
 
+/* TODO: morecore doesn't return block large enough 
+ * if requested size is between 4083 and 4096 */
 void *kmalloc(size_t size)
 {
 	meta_t *b;
 
 	if ((b = find_free_block(size)) == NULL) {
-		kprint("SOS! find_free_block returned NULL!\n\n\n");
-		return NULL;
+		if ((b = morecore(size + META_SIZE)) == NULL)
+			kpanic("kernel heap exhausted"); /* kpanic doesn't return  */
+
+		split_block(b, size);
+		UNMARK_FREE(b);
 	}
-	/* TODO: else allocate more pages */
 
 	return b + 1;
 }
@@ -93,48 +119,31 @@ void *krealloc(void *ptr, size_t size)
 	meta_t *b = GET_BASE(ptr), *tmp;
 	size_t new_size = b->size;
 
-	if (b->prev && IS_FREE(b->prev)) new_size += b->prev->size;
-	if (b->next && IS_FREE(b->next)) new_size += b->next->size;
+	if (b->prev && IS_FREE(b->prev)) new_size += b->prev->size + META_SIZE;
+	if (b->next && IS_FREE(b->next)) new_size += b->next->size + META_SIZE;
 
 	if (new_size >= size) {
 		if (b->prev != NULL && IS_FREE(b->prev)) {
-			tmp = b->prev;
-			tmp->size += b->size;
-			tmp->next = b->next;
-			if (b->next)
-				b->next->prev = tmp;
-
-			/* move b contents to the beginning of new block */
-			memmove(tmp, b, b->size);
-			b = tmp;
+			merge_blocks(b->prev, b);
+			memmove(b->prev + 1, b + 1, b->size);
+			b = b->prev;
 		}
 
 		if (b->next != NULL && IS_FREE(b->next)) {
-			tmp = b->next;
-			b->size += tmp->size;
-			b->next = tmp->next;
-			if (tmp->next)
-				tmp->next->prev = b;
+			merge_blocks(b, b->next);
 		}
-
-		/* split block if possible to save space */
-		meta_t *split;
-		if ((split = split_block(b, size)) != NULL) {
-			tmp->next = b->next;
-			if (b->next)
-				b->next->prev = tmp;
-			tmp->prev = b;
-			b->next = tmp;
-		}
+		split_block(b, size);
 
 	} else {
 		tmp = find_free_block(size);
 		if (!tmp) {
-			/* TODO: allocate more memory */
+			tmp = morecore(size);
+			split_block(tmp, size);
 		}
 
-		memcpy(tmp, b, b->size);
-		MARK_FREE(b); /* TODO: kfree?? */
+		UNMARK_FREE(tmp);
+		memcpy(tmp + 1, b + 1, b->size);
+		kfree(ptr);
 		b = tmp;
 	}
 
@@ -151,21 +160,14 @@ void kfree(void *ptr)
 	MARK_FREE(b);
 
 	if (b->prev != NULL && IS_FREE(b->prev)) {
-		tmp = b->prev;
-		tmp->size += b->size + META_SIZE; /* header is part of the block size too */
-		tmp->next = b->next;
-		if (b->next)
-			b->next->prev = tmp;
-		b = tmp;
+		merge_blocks(b->prev, b);
+		b = b->prev;
 	}
 
-	if (b->next != NULL && IS_FREE(b->next)) {
-		tmp = b->next;
-		b->size += tmp->size + META_SIZE; /* header is part of the block size too */
-		b->next = tmp->next;
-		if (tmp->next)
-			tmp->next->prev = b;
-	}
+	if (b->next != NULL && IS_FREE(b->next))
+		merge_blocks(b, b->next);
+
+	/* TODO: call kfree_frame */
 }
 
 void traverse()
@@ -174,13 +176,14 @@ void traverse()
 
 	kprint("-------------------------------------\n");
 	while (b) {
-		kprint("b 0x%08x b->size %4u free %u b->prev 0x%08x b->next 0x%08x\n",
+		kprint("b 0x%08x b->size %4d free %u b->prev 0x%08x b->next 0x%08x\n",
 				b, b->size, IS_FREE(b), b->prev, b->next);
 		b = b->next;
 	}
 	kprint("-------------------------------------\n");
 }
 
+/* allocate one page (4KiB) of initial memory for kheap */
 void kheap_init()
 {
 	map_page((void*)kalloc_frame(), HEAP_START, P_PRESENT | P_READWRITE);
@@ -191,4 +194,5 @@ void kheap_init()
 	kernel_base->size = 0x1000 - META_SIZE;
 	kernel_base->next = kernel_base->prev = NULL;
 	MARK_FREE(kernel_base);
+	HEAP_BREAK = (uint32_t*)0xd0001000;
 }
