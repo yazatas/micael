cb2c914bc5d5656c416148c32869249d4a8f2826
rficu <rficu@airmail.cc>
2018-03-04 10:37:43 +0200

    Add more complex cooperative multitasking example

--- kernel/kernel/task.c
+++ kernel/kernel/task.c
@@ -1,79 +1,57 @@
 #include <kernel/task.h>
 #include <kernel/kprint.h>
 #include <kernel/mmu.h>
+#include <kernel/kpanic.h>
 
-#include <string.h>
+static task_t *head = NULL;
+static task_t *running = NULL;
 
-static task_t *running_task;
-static task_t main_task;
-static task_t other_task;
-
-static void other_main(void)
+void start_tasking(void)
 {
-    kdebug("hello multitasking world!");
-	kdebug("calculating first 10 fibonacci numbers...");
+	running = head;
 
-	int n = 0, k = 1, i = 0;
 
-	for (; i < 10; ++i) {
-		kprint("%d ", n + k);
-		int tmp = n;
-		n = n + k;
-		k = tmp;
-	}
-	kprint("\n");
+	task_t *tmp = head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = head;
 
-	kdebug("yielding cpu...");
-    yield();
-
-	kdebug("im back! calculating the factorial of 5");
-
-	n = 1;
-	for (int i = 1; i <= 5; ++i) {
-		n *= i;
-	}
-
-	kdebug("factorial of 5 is %d", n);
-	kdebug("this function has reached its end");
-	yield();
+	yield(); /* switch from kmain to next task */
 }
 
-void init_tasking(void)
+void create_task(void(*func)(), uint32_t stack_size, const char *name)
 {
-    asm volatile("movl %%cr3, %%eax \n \
-                  movl %%eax, %0 " : "=r" (main_task.regs.cr3));
+	kdebug("initializing task %s", name);
+
+	task_t *tmp = kcalloc(1, sizeof(task_t));
+
+	tmp->regs.eip = (uint32_t)func;
+	tmp->regs.esp = (uint32_t)kmalloc(0x1000) + 0x1000 - 1; /* TODO: use stack_size */
+	tmp->name = name;
+
+	/* FIXME: all kernel tasks have the same page directory, is this necessary */
+	asm volatile("movl %%cr3, %%eax \n \
+                  movl %%eax, %0 " : "=r" (tmp->regs.cr3));
 
     asm volatile("pushfl \n \
                   movl (%%esp), %%eax \n \
                   movl %%eax, %0 \n \
-                  popfl" : "=r" (main_task.regs.eflags));
+                  popfl" : "=r" (tmp->regs.eflags));
 
-    create_task(&other_task, other_main, main_task.regs.eflags, main_task.regs.cr3);
-    main_task.next  = (task_t*)&other_task;
-    other_task.next = &main_task;
-
-    running_task = &main_task;
-}
-
-
-void create_task(task_t *task, void(*func)(), uint32_t flags, uint32_t page_dir)
-{
-	memset(task, 0, sizeof(task_t));
-
-    task->regs.eflags = flags;
-    task->regs.eip    = (uint32_t)func;
-    task->regs.cr3    = (uint32_t)page_dir;
-    task->regs.esp    = (uint32_t)kmalloc(0x1000) + 0x1000 - 1;
-
-	kdebug("creating task... eip (0x%x) cr3 (0x%x) esp (0x%x)",
-			task->regs.eip, task->regs.cr3, task->regs.esp);
+	tmp->next = head;
+	head = tmp;
 }
 
 void yield(void)
 {
-    task_t *last = running_task;
-    running_task = running_task->next;
+	task_t *prev = running;
+	running = running->next;
 
-	kdebug("switching tasks...");
-	switch_task(&last->regs, &running_task->regs);
+	if (running == NULL) {
+		kdebug("starting from top again");
+		running = head;
+	}
+
+	kdebug("switching from %s to %s", prev->name, running->name);
+	switch_task(&prev->regs, &running->regs);
 }
