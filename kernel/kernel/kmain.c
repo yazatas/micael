60741c96f69231e4e4732ac0c811b382a68e6bd4
rficu <rficu@airmail.cc>
2018-05-08 19:46:44 +0300

    Add PoC user mode processes
    Still a lot of work needs to be done but this is the first step
    towards user mode, syscalls and preemptive multitasking

--- kernel/kernel/kmain.c
+++ kernel/kernel/kmain.c
@@ -7,24 +7,25 @@
 #include <kernel/gdt.h>
 #include <kernel/idt.h>
 #include <kernel/irq.h>
-#include <kernel/mmu.h>
 #include <kernel/kprint.h>
 #include <kernel/kpanic.h>
+
 #include <kernel/task.h>
 #include <kernel/mutex.h>
+
+#include <mm/vmm.h>
+#include <mm/kheap.h>
+
+#include <sched/proc.h>
 
 #include <drivers/timer.h>
 #include <drivers/keyboard.h>
 
 extern uint32_t __kernel_virtual_start,  __kernel_virtual_end;
 extern uint32_t __kernel_physical_start, __kernel_physical_end;
+extern uint32_t __code_segment_start, __code_segment_end;
+extern uint32_t __data_segment_start, __data_segment_end;
 extern uint32_t boot_page_dir; 
-
-mutex_t test_mutex = 0;
-
-void func_1(void);
-void func_2(void);
-void func_3(void);
 
 void kmain(void)
 {
@@ -34,72 +35,28 @@ void kmain(void)
 		   "[kmain] kvirtual kphysical end:   0x%08x 0x%08x",
 		   &__kernel_virtual_start, &__kernel_physical_start,
 		   &__kernel_virtual_end,   &__kernel_physical_end);
+	kdebug("code segment start - end: 0x%08x - 0x%08x",
+			&__code_segment_start, &__code_segment_end);
+	kdebug("data segment start - end: 0x%08x - 0x%08x",
+			&__data_segment_start, &__data_segment_end);
 	kdebug("kpage dir start addr:     0x%08x", &boot_page_dir);
 
-	gdt_init(); idt_init(); irq_init(); 
+	gdt_init(); 
+	idt_init(); 
+	irq_init(); 
 	timer_install(); kb_install();
 	asm ("sti"); /* enable interrupts */
 	vmm_init();
 
-	kthread_create(func_3, 0x500, "func_3");
-	kthread_create(func_2, 0x500, "func_2");
-	kthread_create(func_1, 0x500, "func_1");
-	kthread_start();
+	vmm_list_pde();
+
+	pcb_t *p;
+	if ((p = process_create("haloust")) != NULL) {
+		kdebug("process created successfully");
+		kdebug("pid %u", p->pid);
+	} else {
+		kdebug("failed to create process");
+	}
 
 	for (;;);
-}
-
-void func_1(void)
-{
-	kdebug("started func_1");
-	mutex_lock(&test_mutex);
-	int counter = 0;
-
-	while (1) {
-		kprint("in function 1 %d\n", counter);
-		for (volatile int i = 0; i < 100000000; ++i) {};
-
-		counter++;
-		if (counter == 10) {
-			kprint("counter is 10, release mutex\n");
-			mutex_unlock(&test_mutex);
-		}
-
-		kthread_yield();
-	}
-}
-
-void func_2(void)
-{
-	kdebug("started func_2");
-
-	for (int i = 0; i < 3; ++i) {
-		kprint("in function 2\n");
-		for (volatile int i = 0; i < 60000000; ++i) {};
-		kthread_yield();
-	}
-
-	kthread_delete();
-}
-
-void func_3(void)
-{
-	kdebug("started func_3");
-
-	while (1) {
-		kprint("in function 3\n");
-		kprint("\ttrying to take test_mutex...\n");
-
-		if (mutex_trylock(&test_mutex) == 0) {
-			kprint("\twe have the mutex!\n");
-			for (volatile int i = 0; i < 100000000; ++i) {};
-
-			kprint("\tunlock the mutex again\n");
-			mutex_unlock(&test_mutex);
-		} else {
-			kprint("\tfailed to unlock mutex\n");
-		}
-
-		kthread_yield();
-	}
 }
