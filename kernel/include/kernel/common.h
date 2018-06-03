6c681ef1677149c76e8ec82d3f8526f597a1fe18
rficu <rficu@airmail.cc>
2018-06-03 10:18:00 +0300

    refactor code in common.h and add min/max macros

--- kernel/include/kernel/common.h
+++ kernel/include/kernel/common.h
@@ -6,13 +6,17 @@
 #include <limits.h>
 
 typedef struct isr_regs {
-	uint16_t gs, fs, es, ds;
-	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; /* pusha */
-	uint32_t isr_num, err_num;
-	uint32_t eip, cs, eflags, useresp, ss; /*  pushed by cpu */
+    uint16_t gs, fs, es, ds;
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; /* pusha */
+    uint32_t isr_num, err_num;
+    uint32_t eip, cs, eflags, useresp, ss; /*  pushed by cpu */
 } isr_regs_t;
 
+#define MIN(v1, v2) ((v1 < v2) ? v1 : v2)
+#define MAX(v1, v2) ((v1 < v2) ? v2 : v1)
 #define ROUND_DOWN(addr, boundary) (addr & ~(boundary - 1))
-#define ROUND_UP(addr, bound) ((addr % bound) ? ((addr & ~(bound - 1)) + bound) : (addr))
+#define ROUND_UP(addr,   boundary) ((addr % boundary) ? \
+								   ((addr & ~(boundary - 1)) + boundary) : \
+									(addr))
 
 #endif /* end of include guard: __COMMON_H__ */
