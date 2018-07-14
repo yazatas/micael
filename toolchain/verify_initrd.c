9b28778ea904249030120b2153e3ab50b0ef9ff3
rficu <rficu@airmail.cc>
2018-07-14 08:20:52 +0300

    Add initrd generation and verification programs
    File names still don't work so they must be fix in separate commit

---
+++ toolchain/verify_initrd.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/* how to use this:
+ *
+ * gcc -o verify verify_mkinitrd.c && ./verify <file1> <file2> ... <file4> 
+ *
+ * This program shall verify that the generated initrd is valid.
+ * initrd verification is used only for debugging */
+
+#define HEADER_MAGIC 0x13371338
+#define FILE_MAGIC   0xdeadbeef
+
+typedef struct disk_header {
+    uint8_t  file_count;
+    uint32_t disk_size;
+    uint32_t magic;
+} disk_header_t;
+
+typedef struct file_header {
+    char *file_name;
+    uint32_t file_size;
+    uint32_t magic;
+} file_header_t;
+
+uint32_t get_file_size(FILE *fp)
+{
+    uint32_t fsize = 0;
+
+    fseek(fp, 0L, SEEK_END);
+    fsize = (uint32_t)ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+
+    return fsize;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1) {
+        return -1;
+    }
+
+    FILE *disk_fp = fopen("initrd.bin", "r"), *tmp;
+    char *buffer  = NULL;
+
+    disk_header_t disk_header;
+    file_header_t file_header;
+
+    fread(&disk_header, sizeof(disk_header_t), 1, disk_fp);
+
+    if (disk_header.magic != HEADER_MAGIC) {
+        fprintf(stderr, "invalid magic number for disk header!\n");
+        return -1;
+    }
+
+    fprintf(stderr, "file count: %u\ndisk size:  %u\n", 
+            disk_header.file_count, disk_header.disk_size);
+
+    fseek(disk_fp, sizeof(disk_header_t), SEEK_SET);
+
+    for (int i = 1; i < argc; ++i) {
+        fread(&file_header, sizeof(file_header_t), 1, disk_fp);
+
+        if (file_header.magic != FILE_MAGIC) {
+            fprintf(stderr, "invalid magic number for file header!\n");
+            return -1;
+        }
+
+        fprintf(stderr, "file info:\n\
+                \tname:  %s\n\
+                \tsize:  %u\n\
+                \tmagic: 0x%x\n\n", NULL, file_header.file_size, file_header.magic);
+
+        FILE *verify_fp  = fopen(argv[i], "r");
+        char *initrd_buf = malloc(file_header.file_size);
+        char *verify_buf = malloc(get_file_size(verify_fp));
+
+        fread(initrd_buf, file_header.file_size, 1, disk_fp);
+        fread(verify_buf, file_header.file_size, 1, verify_fp);
+
+        for (uint32_t k = 0; k < file_header.file_size; ++k) {
+            if (initrd_buf[k] != verify_buf[k]) {
+                fprintf(stderr, "byte at index %u does NOT match!\n", k);
+                return -1;
+            }
+        }
+
+        fprintf(stderr, "file at index %u (size %u) OK!\n\n", i, file_header.file_size);
+
+        fclose(verify_fp);
+        free(initrd_buf);
+        free(verify_buf);
+    }
+        
+    fclose(disk_fp);
+    return 0;
+}
