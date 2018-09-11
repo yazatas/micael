20db9c6dee027061fb029fd58ad019c6375002a9
rficu <rficu@airmail.cc>
2018-09-11 20:59:09 +0300

    add intrusive lists

--- kernel/lib/list.c
+++ kernel/lib/list.c
@@ -1,5 +1,23 @@
 #include <lib/list.h>
 
+void list_init(list_head_t *s)
+{
+    s->next = s;
+    s->prev = s;
+}
+
+void list_init_null(list_head_t *s)
+{
+    s->next = NULL;
+    s->prev = NULL;
+}
+
+void list_remove(list_head_t *s)
+{
+    s->next->prev = s->prev;
+    s->prev->next = s->next;
+}
+
 /* insert node between next and prev */
 void list_insert(list_head_t *new, list_head_t *next, list_head_t *prev)
 {
@@ -19,6 +37,24 @@ void list_append(list_head_t *s, list_head_t *t)
     s->next = t;
 }
 
+/* s points to list end, add t at the end */
+void list_insert_tail(list_head_t *s, list_head_t *t)
+{
+    s->next = t;
+    t->prev = s;
+
+    t->next = NULL;
+}
+
+/* s points to list head, add t at the beginning */
+void list_insert_head(list_head_t *s, list_head_t *t)
+{
+    s->prev = t;
+    t->next = s;
+
+    t->prev = NULL;
+}
+
 /* insert t before s */
 void list_prepend(list_head_t *s, list_head_t *t)
 {
@@ -28,16 +64,3 @@ void list_prepend(list_head_t *s, list_head_t *t)
     s->prev->next = t;
     s->prev = t;
 }
-
-void list_remove(list_head_t *s)
-{
-    s->next->prev = s->prev;
-    s->prev->next = s->next;
-}
-
-void list_init(list_head_t *s)
-{
-    s->next = s;
-    s->prev = s;
-}
-
