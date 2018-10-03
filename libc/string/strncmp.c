13d9564d0c43c03f87b9b1ac262bd1c3c997f7f4
rficu <rficu@airmail.cc>
2018-10-03 20:31:36 +0300

    add strncmp for libc

--- libc/string/strncmp.c
+++ libc/string/strncmp.c
@@ -1,12 +1,13 @@
 #include <string.h>
 
-int strncmp(const char *s1, const char *s2, size_t n)
+int strncmp(const char *restrict s1, const char *restrict s2, size_t len)
 {
-	for (size_t i = 0; i < n; ++i) {
-		if (s1[i] < s2[i])
-			return -1;
-		else if (s1[i] > s2[i])
-			return 1;
-	}
-	return 0;
+    for (size_t i = 0; s1[i] != '\0' && s2[i] != '\0' && i < len; ++i) {
+        if (s1[i] < s2[i])
+            return -1;
+        else if (s1[i] > s2[i])
+            return 1;
+    }
+    return 0;
+
 }
