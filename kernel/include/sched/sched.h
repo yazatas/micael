6ea5e6e81e36f2e73cf4bd274f3a937c40f9edfa
rficu <rficu@airmail.cc>
2019-01-09 20:35:17 +0200

    Create run_queue for tasks using list_head_t
    Previous implementation had some problems, now fixed

--- kernel/include/sched/sched.h
+++ kernel/include/sched/sched.h
@@ -6,11 +6,16 @@
 
 void sched_init(void);
 void sched_start(void) __noreturn;
+void sched_switch(void) __noreturn;
 void sched_task_schedule(task_t *t);
 
 /* TODO: comment */
 task_t *sched_get_current(void);
+task_t *sched_get_init(void);
 void sched_enter_userland(void *eip, void *esp) __noreturn;
+
+
+/* TODO: remove */
 void sched_print_tasks(void);
 
 #endif /* end of include guard: __SCHED_H__ */
