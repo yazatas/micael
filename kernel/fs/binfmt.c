dd55f5d3bbb8ce26885dee941f2f54c82645e6c5
rficu <rficu@airmail.cc>
2019-03-02 18:01:19 +0200

    Rewrite the FOREACH method for lists
    Previous version used NULL pointers to determine end of list
    which caused some problems every now and then.

--- kernel/fs/binfmt.c
+++ kernel/fs/binfmt.c
@@ -2,17 +2,16 @@
 #include <kernel/kpanic.h>
 #include <mm/heap.h>
 
+static list_head_t loaders;
+
 typedef struct {
     binfmt_loader_t loader;
     list_head_t list;
 } binfmt_t;
 
-static binfmt_t loaders;
-
 void binfmt_init(void)
 {
-    loaders.loader = NULL;
-    list_init_null(&loaders.list);
+    list_init(&loaders);
 }
 
 void binfmt_add_loader(binfmt_loader_t loader)
@@ -20,8 +19,8 @@ void binfmt_add_loader(binfmt_loader_t loader)
     binfmt_t *bfmt = kmalloc(sizeof(binfmt_t));
     bfmt->loader = loader;
 
-    list_init_null(&bfmt->list);
-    list_append(&loaders.list, &bfmt->list);
+    list_init(&bfmt->list);
+    list_append(&loaders, &bfmt->list);
 }
 
 bool binfmt_load(file_t *file, int argc, char **argv)
@@ -29,8 +28,8 @@ bool binfmt_load(file_t *file, int argc, char **argv)
     list_head_t *iter = NULL;
     binfmt_t *loader  = NULL;
 
-    FOREACH(&loaders.list, iter) {
-        loader = container_of(iter, binfmt_t, list);
+    FOREACH(loaders, l) {
+        loader = container_of(l, binfmt_t, list);
 
         if (loader->loader && loader->loader(file, argc, argv))
             kpanic("binfmt load_bin returned!");
