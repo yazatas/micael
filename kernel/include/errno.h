672959a4d25bda67f336b757c28c1fabedb9d0b3
rficu <rficu@airmail.cc>
2019-03-08 08:15:01 +0200

    Add kstrerror
    Re-implementation of the libc's strerror but for kernel

--- kernel/include/errno.h
+++ kernel/include/errno.h
@@ -3,15 +3,18 @@
 
 int errno;
 
-#define ENOSPC 1
-#define ENOENT 2
-#define EINVAL 3
-#define EEXIST 4
-#define ENOMEM 5
-#define EBUSY  6
-#define E2BIG  7  /* Argument list too long */
-#define ESPIPE 29 /* Illegal seek */
-#define ENOSYS 38 /* Funcion not implemented */
+#define ENOSPC   1 /* No space left on device */
+#define ENOENT   2 /* No such file or directory */
+#define EINVAL   3 /* Invalid value */
+#define EEXIST   4 /* File exists */
+#define ENOMEM   5 /* Out of memory */
+#define EBUSY    6 /* Device busy */
+#define E2BIG    7 /* Argument list too long */
+#define ESPIPE   8 /* Illegal seek */
+#define ENOSYS   9 /* Funcion not implemented */
+#define ENOTDIR 10 /* Not a directory */
+#define ENOTSUP 11 /* Operation not supported */
 
+#define EMAX    12 /* Used by the kstrerror() */
 
 #endif /* __ERRNO_H__ */
