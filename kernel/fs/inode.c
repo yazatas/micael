2b9653e94308b595761c6628a6b6e836b19950d2
rficu <rficu@airmail.cc>
2019-03-10 16:26:14 +0200

    Add inode allocation/deallocation routines

--- kernel/fs/inode.c
+++ kernel/fs/inode.c
@@ -1,14 +1,69 @@
+#include <fs/file.h>
 #include <fs/inode.h>
-#include <stddef.h>
+#include <kernel/kpanic.h>
+#include <mm/cache.h>
+#include <errno.h>
 
-inode_t *inode_alloc()
+static cache_t *inode_cache     = NULL;
+static cache_t *inode_ops_cache = NULL;
+static cache_t *file_ops_cache  = NULL;
+
+int inode_init(void)
 {
-    return NULL;
+    if ((inode_cache = cache_create(sizeof(inode_t), C_NOFLAGS)) == NULL)
+        kpanic("failed to initialize slab cache for inodes!");
+
+    if ((inode_ops_cache = cache_create(sizeof(inode_ops_t), C_NOFLAGS)) == NULL)
+        kpanic("failed to initialize slab cache for inode ops!");
+
+    if ((file_ops_cache = cache_create(sizeof(file_ops_t), C_NOFLAGS)) == NULL)
+        kpanic("failed to initialize slab cache for file ops!");
+
+    return 0;
+}
+
+inode_t *inode_alloc_empty(uint32_t flags)
+{
+    inode_t *ino = NULL;
+
+    if (((ino        = cache_alloc_entry(inode_cache,     C_NOFLAGS)) == NULL) ||
+        ((ino->f_ops = cache_alloc_entry(file_ops_cache,  C_NOFLAGS)) == NULL) ||
+        ((ino->i_ops = cache_alloc_entry(inode_ops_cache, C_NOFLAGS)) == NULL))
+    {
+        if (ino) {
+            if (ino->i_ops)
+                cache_dealloc_entry(inode_ops_cache, ino->i_ops, C_NOFLAGS);
+            cache_dealloc_entry(inode_cache, ino, C_NOFLAGS);
+        }
+
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    /* TODO: get these from sched? */
+    ino->i_uid   = -1;
+    ino->i_gid   = -1;
+
+    ino->i_ino   = -1;
+    ino->i_size  = -1;
+    ino->i_mask  = 0;
+    ino->i_count = 1;
+    ino->i_flags = flags;
+
+    return ino;
 }
 
 int inode_dealloc(inode_t *ino)
 {
-    (void)ino;
+    if (!ino)
+        return -EINVAL;
+
+    if (ino->i_count > 1)
+        return -EBUSY;
+
+    cache_dealloc_entry(inode_ops_cache, ino->i_ops, C_NOFLAGS);
+    cache_dealloc_entry(file_ops_cache,  ino->f_ops, C_NOFLAGS);
+    cache_dealloc_entry(inode_cache,     ino,        C_NOFLAGS);
 
     return 0;
 }
