53b387ea4b5a0e85e5583865e3aef3d436bc11b1
rficu <rficu@airmail.cc>
2019-03-21 20:21:21 +0200

    Add small toolchain for the project
    This is just for testing the user space: crappy calculator,
    echo programn and a small shell utility

---
+++ toolchain/programs/src/init.c
@@ -0,0 +1,11 @@
+typedef unsigned int size_t;
+
+int write(int fd, char *buf, size_t size);
+int execv(const char *path, const char *argv[]);
+
+int main(int argc, char **argv)
+{
+    if (execv("/sbin/dsh", (void *)0) == -1)
+        write(1, "failed to open /sbin/dsh", 24);
+    while (1);
+}
