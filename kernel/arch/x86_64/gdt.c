84b29db685307e3930e947b7e73b1c5469b84430
rficu <rficu@airmail.cc>
2019-04-11 18:38:47 +0300

    arch/x86_64: Initialize new GDT after boot

--- kernel/arch/x86_64/gdt.c
+++ kernel/arch/x86_64/gdt.c
@@ -1,53 +1,23 @@
-#include <arch/x86_64/gdt.h>
 #include <kernel/gdt.h>
 #include <kernel/kprint.h>
 #include <kernel/util.h>
 
 extern unsigned long boot_stack;
-/* extern unsigned long gdt_ptr; */
-extern unsigned long *gdt_table_start;
-extern struct tss_ptr_t tss_ptr;
 
-void gdt_set_gate(uint32_t base, uint32_t limit, uint8_t access,
-                   uint8_t gran, struct gdt_entry_t *entry)
-{
-	entry->base_low    = base & 0xffff;
-	entry->base_middle = (base >> 16) & 0xff;
-	entry->base_high   = (base >> 24) & 0xff;
-
-	entry->limit_low   = limit & 0xffff;
-	entry->granularity = (limit >> 16) & 0xffff;
-	entry->granularity |= gran & 0xf0;
-	entry->access      = access;
-}
+static uint64_t GDT[5] = { 0 };
+struct gdt_ptr_t gdt_ptr;
+struct tss_ptr_t tss_ptr;
 
 void gdt_init(void)
 {
-    /* on x86_64, the GDT has been initialized during boot and we only need to 
-     * initialize the TSS here */
+    GDT[0] = 0UL;
+    GDT[1] = 0x00a0980000000000UL; /* kernel code */
+    GDT[2] = 0x00c0920000000000UL; /* kernel data */
+    GDT[3] = 0x00a0f80000000000UL; /* user code */
+    GDT[4] = 0x00c0f20000000000UL; /* user data */
 
-    /* gdt_set_gate(0, 0, GDT_64BIT, GDT_PRESENT | GDT_CPL0   | GDT_EXEC, */ 
-    /*         (struct gdt_entry_t *)&gdt_table_start[1]); */
-    /* gdt_set_gate(0, 0, GDT_64BIT, GDT_PRESENT | GDT_CPL0   | GDT_RDWR, */
-    /*         (struct gdt_entry_t *)&gdt_table_start[2]); */
-    /* gdt_set_gate(0, 0, GDT_64BIT, GDT_PRESENT | GDT_CPL3   | GDT_EXEC, */
-    /*         (struct gdt_entry_t *)&gdt_table_start[3]); */
-    /* gdt_set_gate(0, 0, GDT_64BIT, GDT_PRESENT | GDT_CPL3   | GDT_RDWR, */
-    /*         (struct gdt_entry_t *)&gdt_table_start[4]); */
-    /* /1* gdt_set_gate(0, 0, GDT_64BIT, GDT_PRESENT | GDT_ACCESS | GDT_EXEC, *1/ */
-    /* /1*         (struct gdt_entry_t *)&gdt_table_start[5]); *1/ */
+    gdt_ptr.limit = sizeof(GDT) - 1;
+    gdt_ptr.base  = (uint64_t)GDT;
 
-    /* gdt_flush(gdt_flush); */
-
-	/* gdt_set_gate(0, 0xfffff, 0x9a, 0xcf, &GDT[1]);          /1* code segment *1/ */
-	/* gdt_set_gate(0, 0xfffff, 0x92, 0xcf, &GDT[2]);          /1* data segment *1/ */
-	/* gdt_set_gate(0, 0xfffff, 0xfa, 0xcf, &GDT[3]);          /1* user mode code segment *1/ */
-	/* gdt_set_gate(0, 0xfffff, 0xf2, 0xcf, &GDT[4]);          /1* user mode data segment *1/ */
-	/* gdt_set_gate(tss_base, tss_limit, 0x89, 0x40, &GDT[5]); /1* tss segment *1/ */
-
-	/* kmemset(&tss_ptr, 0, sizeof(struct tss_ptr_t)); */
-	/* tss_ptr.ss0  = SEG_KERNEL_DATA;       /1* kernel data segment offset *1/ */
-	/* tss_ptr.esp0 = (uint32_t)&boot_stack; /1* kernel stack *1/ */
-	/* gdt_flush(gdt_ptr); */
-	/* tss_flush(&tss_ptr); */
+    gdt_flush(&gdt_ptr);
 }
--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -2,6 +2,7 @@
 #define __GDT_H__
 
 #include <sys/types.h>
+#include <kernel/common.h>
 
 #define DPL_KERNEL 0x00
 #define DPL_USER   0x03
@@ -16,7 +17,11 @@
 
 struct gdt_ptr_t {
     uint16_t limit;
+#ifdef __x86_64__
+    uint64_t base;
+#else
     uint32_t base;
+#endif
 } __attribute__((packed));
 
 struct gdt_entry_t {
@@ -24,13 +29,48 @@ struct gdt_entry_t {
     uint16_t base_low;
     uint8_t  base_middle;
     uint8_t  access;
-	uint8_t  granularity;
+    uint8_t  granularity;
     uint8_t  base_high;
-} __attribute__((packed)); 
+} __attribute__((packed));
+
+struct tss_ptr_t {
+#ifndef __x86_64__
+    uint32_t rsrvd;
+    uint32_t rsp0_low, rsp0_high;
+
+    uint32_t rsp1_low, rsp1_high;
+    uint32_t rsp2_low, rsp2_high;
+
+    uint32_t rsrvd_2,  rsrvd_3;
+
+    uint32_t ist1_low, ist1_high;
+    uint32_t ist2_low, ist2_high;
+    uint32_t ist3_low, ist3_high;
+    uint32_t ist4_low, ist4_high;
+    uint32_t ist5_low, ist5_high;
+    uint32_t ist6_low, ist6_high;
+    uint32_t ist7_low, ist7_high;
+
+    uint32_t rsrvd_4,  rsrvd_5;
+    uint16_t iopb,     rsrvd_6;
+#else
+    uint32_t prev_tss; /* for hw task switching, not supported */
+    uint32_t esp0;     /* The stack pointer to load when we change to kernel mode */
+    uint32_t ss0;      /* The stack segment to load when we change to kernel mode */
+
+    /* unused */
+    uint32_t esp1, ss1, esp2, ss2, cr3, eip, eflags;
+    uint32_t eax,  ecx, edx,  ebx, esp, ebp, esi, edi, trap;
+    uint32_t es,   cs,  ss,   ds,  fs,  gs,  ldt, iomap_base;
+#endif
+} __attribute__((packed));
 
 void tss_flush();
 void gdt_flush();
-
 void gdt_init(void);
+
+// defined in arch/*/gdt.c
+extern struct gdt_ptr_t gdt_ptr;
+extern struct tss_ptr_t tss_ptr;
 
 #endif /* end of include guard: __GDT_H__ */
