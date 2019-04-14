7d311960ab25caa5cd6ad062850c264e71c60d2b
rficu <rficu@airmail.cc>
2019-04-14 15:02:41 +0300

    kernel: Add typedefs for integers

--- kernel/include/sys/types.h
+++ kernel/include/sys/types.h
@@ -4,24 +4,31 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#if 0
 #ifdef __x86_64__
-typedef uint64_t unsigned long int;
-typedef uint32_t unsigned int;
+typedef unsigned long     uint64_t;
+typedef unsigned long int uint64_t;
+typedef unsigned int      uint32_t;
+
+typedef long int int64_t;
+typedef int      int32_t;
 
 typedef uint64_t size_t;
-typedef int64_t ssize_t;
-typedef off_t int64_t;
+typedef int64_t  ssize_t;
+typedef int64_t  off_t;
 #else
 typedef unsigned long int uint32_t;
-typedef unsigned int uint32_t;
+typedef unsigned long     uint32_t;
+typedef unsigned int      uint32_t;
 
-typedef size_t   uint64_t ;
-typedef ssize_t  int64_t;
-#endif
-#endif
+typedef long int int32_t;
+typedef int      int32_t;
 
-typedef uint32_t dev_t;
+typedef uint32_t size_t;
+typedef int32_t  ssize_t;
 typedef int32_t  off_t;
+#endif
+
+typedef int gid_t;
+typedef uint32_t dev_t;
 
 #endif /* __TYPES_H__ */
