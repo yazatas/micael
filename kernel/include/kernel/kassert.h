179adecdafa240161f651655c1b1ee033c2a4c71
rficu <rficu@airmail.cc>
2019-06-28 17:13:26 +0300

    kernel: Fix #ifdef check in kassert.h

--- kernel/include/kernel/kassert.h
+++ kernel/include/kernel/kassert.h
@@ -1,14 +1,13 @@
 #ifndef __KASSERT_H__
 #define __KASSERT_H__
 
-#ifndef NDEBUG
+#ifdef NDEBUG
 #define kassert(cond)
 #else
 #define kassert(cond) __kernel_assert(cond)
 #define __kernel_assert(cond) \
     if (!(cond)) {            \
         kprint("assertion \"%s\" failed at %s:%d\n", #cond, __FILE__, __LINE__); \
-        ktrace(); \
         while (1); \
     }
 #endif
