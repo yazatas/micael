dd33005693eb7a1a2f1f1ba2814123ecabeedb2f
rficu <rficu@airmail.cc>
2019-06-30 11:20:19 +0300

    mm: Create page array for physical memory

--- kernel/mm/page.c
+++ kernel/mm/page.c
@@ -6,6 +6,7 @@
 #include <lib/list.h>
 #include <mm/bootmem.h>
 #include <mm/heap.h>
+#include <mm/mmu.h>
 #include <mm/page.h>
 #include <mm/slab.h>
 #include <errno.h>
@@ -27,10 +28,11 @@ typedef struct mm_zone {
     mm_block_t blocks[BUDDY_MAX_ORDER];
 } mm_zone_t;
 
-static mm_zone_t zone_dma;
-static mm_zone_t zone_normal;
-static mm_zone_t zone_high;
+static mm_zone_t  zone_dma;
+static mm_zone_t  zone_normal;
+static mm_zone_t  zone_high;
 static mm_cache_t *mm_block_cache;
+static page_t     *page_array;
 
 static inline mm_zone_t *__get_zone(unsigned long start, unsigned long end)
 {
@@ -134,100 +136,11 @@ static unsigned long __split_block(mm_zone_t *zone, unsigned req_order, unsigned
     return split_start;
 }
 
-void mmu_zones_init(void *arg)
-{
-    zone_dma.name    = "MM_ZONE_DMA";
-    zone_normal.name = "MM_ZONE_NORMAL";
-    zone_high.name   = "MM_ZONE_HIGH";
-
-    zone_dma.page_count    = 0;
-    zone_normal.page_count = 0;
-    zone_high.page_count   = 0;
-
-    for (size_t i = 0; i < BUDDY_MAX_ORDER; ++i) {
-        zone_dma.blocks[i].start    = 0;
-        zone_dma.blocks[i].end      = 0;
-
-        zone_normal.blocks[i].start = 0;
-        zone_normal.blocks[i].end   = 0;
-
-        zone_high.blocks[i].start   = 0;
-        zone_high.blocks[i].end     = 0;
-
-        list_init_null(&zone_dma.blocks[i].list);
-        list_init_null(&zone_normal.blocks[i].list);
-        list_init_null(&zone_high.blocks[i].list);
-    }
-
-    if ((mm_block_cache = mmu_cache_create(sizeof(mm_block_t), MM_NO_FLAG)) == NULL)
-        kpanic("Failed to allocate mm_block_cache for PFA");
-
-    /* initialize memory zones using the multiboot memory map */
-    multiboot2_map_memory(arg, mmu_claim_range);
-
-    /* We have now mapped all memory for the PFA but during boot
-     * we allocated memory pages from boot memory manager.
-     *
-     * We must now "claim" these pages and set them as occupied
-     * to prevent reallocation. The boot process is very straightforward right now:
-     *  - alloc page for heap
-     *  - alloc 2 pages for slab
-     *  - alloc slab cache for pfa
-     *  -> done
-     *
-     * This means that we must mark the these three pages (found at the start of DMA zone)
-     * as occupied in the zone (and if they're part of a larger block, split the block) */
-    int entries = 0;
-    struct bootmem_mmap **map = mmu_bootmem_release(&entries);
-
-    /* TODO: i need to rethink this.. */
-}
-
-void mmu_claim_range(unsigned long address, size_t len)
-{
-    mm_zone_t *zone = __get_zone(address, address + len);
-
-    if (zone != NULL) {
-        /* round up and down to get usable boundaries */
-        __claim_range(
-            zone,
-            ROUND_UP(address, PAGE_SIZE),
-            ROUND_DOWN(address + len, PAGE_SIZE)
-        );
-    }
-
-    /* There are two possibilities for the range to be NULL:
-     * - the address range given was invalid (rarely the case)
-     * - the address range given overlaps two (or more) zones
-     *
-     * We must check here which is the case and if it's the overlapping case,
-     * deal with the error by splitting the range into two smaller ranges */
-    if (address < MM_ZONE_NORMAL_START && address + len > MM_ZONE_DMA_END) {
-        __claim_range(
-            &zone_dma,
-            ROUND_UP(address, PAGE_SIZE),
-            MM_ZONE_DMA_END
-        );
-
-        __claim_range(
-            &zone_normal,
-            MM_ZONE_NORMAL_START,
-            ROUND_DOWN(address + len, PAGE_SIZE)
-        );
-    }
-}
-
-void mmu_claim_page(unsigned long address)
-{
-    mmu_claim_range(address, PAGE_SIZE);
-}
-
-unsigned long mmu_page_alloc(unsigned memzone)
-{
-    return mmu_block_alloc(memzone, 0);
-}
-
-unsigned long mmu_block_alloc(unsigned memzone, unsigned order)
+/* Allocate block of memory from requested zone
+ *
+ * This function can fail and it return INVALID_ADDRESS on error
+ * and pointer to valid block of memory on succes */
+static unsigned long __alloc_mem(unsigned memzone, unsigned order)
 {
     /* TODO: this is temporary, pfa and bootmem need better cooperation but
      * right now I'll focus my attention to finalizing x86_64 support */
@@ -278,6 +191,134 @@ unsigned long mmu_block_alloc(unsigned memzone, unsigned order)
     return INVALID_ADDRESS;
 }
 
+void mmu_zones_init(void *arg)
+{
+    zone_dma.name    = "MM_ZONE_DMA";
+    zone_normal.name = "MM_ZONE_NORMAL";
+    zone_high.name   = "MM_ZONE_HIGH";
+
+    zone_dma.page_count    = 0;
+    zone_normal.page_count = 0;
+    zone_high.page_count   = 0;
+
+    for (size_t i = 0; i < BUDDY_MAX_ORDER; ++i) {
+        zone_dma.blocks[i].start    = 0;
+        zone_dma.blocks[i].end      = 0;
+
+        zone_normal.blocks[i].start = 0;
+        zone_normal.blocks[i].end   = 0;
+
+        zone_high.blocks[i].start   = 0;
+        zone_high.blocks[i].end     = 0;
+
+        list_init_null(&zone_dma.blocks[i].list);
+        list_init_null(&zone_normal.blocks[i].list);
+        list_init_null(&zone_high.blocks[i].list);
+    }
+
+    if ((mm_block_cache = mmu_cache_create(sizeof(mm_block_t), MM_NO_FLAG)) == NULL)
+        kpanic("Failed to allocate mm_block_cache for PFA");
+
+    /* initialize memory zones using the multiboot memory map */
+    multiboot2_map_memory(arg, mmu_claim_range);
+
+    /* We have now mapped all memory for the PFA but during boot
+     * we allocated memory pages from boot memory manager.
+     *
+     * We must now "claim" these pages and set them as occupied
+     * to prevent reallocation. The boot process is very straightforward right now:
+     *  - alloc page for heap
+     *  - alloc 2 pages for slab
+     *  - alloc slab cache for pfa
+     *  -> done
+     *
+     * This means that we must mark the these three pages (found at the start of DMA zone)
+     * as occupied in the zone (and if they're part of a larger block, split the block) */
+    /* TODO: i need to rethink this.. */
+    int entries = 0;
+    struct bootmem_mmap **map = mmu_bootmem_release(&entries);
+
+    /* Create page array for all physical memory (current max: 2GB bytes)
+     *
+     * Each page maps 4096 bytes of memory so we need 2GB / 4096 == 0x800000
+     * entries to hold the entire page array in memory and each entry is sizeof(page_t)
+     * bytes of memory so in total we need 0x800000 * sizeof(page_t) which is 10MB of memory
+     * but due to lack of granularity we need to allocate a much larger block
+     *
+     * Use the internal allocation function to allocate the block because currently
+     * the page_array points to NULL */
+    unsigned long pa_mem = __alloc_mem(MM_ZONE_NORMAL, 12);
+
+    if (pa_mem == INVALID_ADDRESS)
+        kpanic("failed to allocate memory for page array");
+
+    page_array = mmu_p_to_v(pa_mem);
+
+    kdebug("0x%x", pa_mem, pa_mem >> PAGE_SHIFT);
+
+    page_array[pa_mem >> PAGE_SHIFT].type  = MM_PT_IN_USE;
+    page_array[pa_mem >> PAGE_SHIFT].order = 12;
+}
+
+void mmu_claim_range(unsigned long address, size_t len)
+{
+    mm_zone_t *zone = __get_zone(address, address + len);
+
+    if (zone != NULL) {
+        /* round up and down to get usable boundaries */
+        __claim_range(
+            zone,
+            ROUND_UP(address, PAGE_SIZE),
+            ROUND_DOWN(address + len, PAGE_SIZE)
+        );
+    }
+
+    /* There are two possibilities for the range to be NULL:
+     * - the address range given was invalid (rarely the case)
+     * - the address range given overlaps two (or more) zones
+     *
+     * We must check here which is the case and if it's the overlapping case,
+     * deal with the error by splitting the range into two smaller ranges */
+    if (address < MM_ZONE_NORMAL_START && address + len > MM_ZONE_DMA_END) {
+        __claim_range(
+            &zone_dma,
+            ROUND_UP(address, PAGE_SIZE),
+            MM_ZONE_DMA_END
+        );
+
+        __claim_range(
+            &zone_normal,
+            MM_ZONE_NORMAL_START,
+            ROUND_DOWN(address + len, PAGE_SIZE)
+        );
+    }
+}
+
+void mmu_claim_page(unsigned long address)
+{
+    mmu_claim_range(address, PAGE_SIZE);
+}
+
+unsigned long mmu_page_alloc(unsigned memzone)
+{
+    return mmu_block_alloc(memzone, 0);
+}
+
+unsigned long mmu_block_alloc(unsigned memzone, unsigned order)
+{
+    unsigned long mem = __alloc_mem(memzone, order);
+
+    if (mem == INVALID_ADDRESS)
+        return INVALID_ADDRESS;
+
+    unsigned pfn = mem >> PAGE_SHIFT;
+
+    page_array[pfn].type  = MM_PT_IN_USE;
+    page_array[pfn].order = order;
+
+    return mem;
+}
+
 int mmu_block_free(unsigned long address, unsigned order)
 {
     if (!PAGE_ALIGNED(address))
@@ -287,6 +328,8 @@ int mmu_block_free(unsigned long address, unsigned order)
 
     if (zone == NULL)
         return -ENXIO;
+
+    page_array[address >> PAGE_SHIFT].type = MM_PT_FREE;
 
     return __zone_add_block(zone, address, order);
 }
