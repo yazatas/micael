156276b6959b9749d52cce936fad3a68af9328cd
rficu <rficu@airmail.cc>
2019-07-08 20:41:58 +0300

    arch/x86_64: Identity map the first 2GB of memory

--- kernel/arch/x86_64/mmu.c
+++ kernel/arch/x86_64/mmu.c
@@ -3,9 +3,9 @@
 #include <mm/page.h>
 #include <sys/types.h>
 
-static uint64_t __pml4[512]  __attribute__((aligned(0x1000)));
-static uint64_t __pdpt[512]  __attribute__((aligned(0x1000)));
-static uint64_t __pd[512]    __attribute__((aligned(0x1000)));
+static uint64_t __pml4[512]   __attribute__((aligned(0x1000)));
+static uint64_t __pdpt[512]   __attribute__((aligned(0x1000)));
+static uint64_t __pd[512 * 2] __attribute__((aligned(0x1000)));
 
 static uint64_t __pml4_;
 
@@ -55,11 +55,14 @@ int mmu_native_init(void)
     /* map also the lower part of the address space so our boot stack still works
      * It only has to work until we switch to init task */
     __pml4[PML4_ATOEI(KVSTART)] = __pml4[0] = V_TO_P(&__pdpt) | MM_PRESENT | MM_READWRITE;
-    __pdpt[PDPT_ATOEI(KVSTART)] = __pdpt[0] = V_TO_P(&__pd)   | MM_PRESENT | MM_READWRITE;
 
-    /* map the first 1GB of address space */
-    for (size_t i = 0; i < 512; ++i) {
+    /* map the first 2GB of address space */
+    for (size_t i = 0; i < 512 * 2; ++i) {
         __pd[i] = (i * (1 << 21)) | MM_PRESENT | MM_READWRITE | MM_2MB;
+    }
+
+    for (size_t i = 0; i < 2; ++i) {
+        __pdpt[PDPT_ATOEI(KVSTART) + i] = __pdpt[i] = V_TO_P(&__pd[i * 512]) | MM_PRESENT | MM_READWRITE;
     }
 
     native_set_cr3(V_TO_P(__pml4));
