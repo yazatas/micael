7676221ed389fd7881b5b7ea412cf987ecedeeca
rficu <rficu@airmail.cc>
2019-07-27 19:32:47 +0300

    mm: Add generic functions for page directory creation/duplication
    The actual implementationo for duplication on x86_64 is still missing

--- kernel/arch/x86_64/mmu.c
+++ kernel/arch/x86_64/mmu.c
@@ -5,14 +5,15 @@
 #include <mm/page.h>
 #include <sys/types.h>
 
-static uint64_t __pml4[512]   __attribute__((aligned(0x1000)));
-static uint64_t __pdpt[512]   __attribute__((aligned(0x1000)));
-static uint64_t __pd[512 * 2] __attribute__((aligned(0x1000)));
+static uint64_t __pml4[512]   __attribute__((aligned(PAGE_SIZE)));
+static uint64_t __pdpt[512]   __attribute__((aligned(PAGE_SIZE)));
+static uint64_t __pd[512 * 2] __attribute__((aligned(PAGE_SIZE)));
 
 static uint64_t __pml4_;
 
 #define KPSTART 0x0000000000100000
 #define KVSTART 0xffffffff80100000
+#define KPML4I  511
 
 #define PML4_ATOEI(addr) (((addr) >> 39) & 0x1FF)
 #define PDPT_ATOEI(addr) (((addr) >> 30) & 0x1FF)
@@ -128,4 +129,78 @@ unsigned long mmu_native_v_to_p(void *vaddr)
 void *mmu_native_p_to_v(unsigned long paddr)
 {
     return native_p_to_v(paddr);
+}
+
+void *mmu_native_build_dir(void)
+{
+    uint64_t pml4_p;
+    uint64_t *pml4_v;
+
+    if ((pml4_p = mmu_page_alloc(MM_ZONE_NORMAL)) == INVALID_ADDRESS)
+        return NULL;
+
+    pml4_v = native_p_to_v(pml4_p);
+
+    for (size_t i = 0; i < 511; ++i) {
+        pml4_v[i] = ~MM_PRESENT;
+    }
+
+    pml4_v[0] = pml4_v[PML4_ATOEI(KVSTART)] = __pml4[PML4_ATOEI(KVSTART)];
+
+    return pml4_v;
+}
+
+void *mmu_native_duplicate_dir(void)
+{
+#if 0
+    kdebug("starting address space duplication..");
+
+    uint32_t *pdir   = mmu_build_pagedir();
+    uint32_t *d_iter = (uint32_t *)0xfffff000;
+    uint32_t *t_iter = NULL;
+    uint32_t *tbl_v  = NULL;
+
+    for (size_t i = 0; i < KSTART; ++i) {
+        if (MM_TEST_FLAG(d_iter[i], MM_PRESENT)) {
+            pdir[i] = mmu_alloc_page() | (d_iter[i] & 0xfff);
+            t_iter  = ((uint32_t *)0xffc00000) + (0x400 * i);
+
+            for (size_t k = 0; k < 1024; ++k) {
+                if (MM_TEST_FLAG(t_iter[k], MM_PRESENT)) {
+                    /* TODO: rewrite this!! */
+                    void *new_addr = mmu_alloc_addr(1);
+                    if (tbl_v != NULL)
+                        mmu_free_addr(tbl_v, 1);
+                    tbl_v = new_addr;
+
+                    mmu_map_page((void *)pdir[i], tbl_v, MM_PRESENT | MM_READWRITE);
+
+                    t_iter[k] &= ~MM_READWRITE; /* rw must be cleared explicitly */
+                    t_iter[k] |= (MM_COW | MM_READONLY);
+                    tbl_v[k]   = t_iter[k];
+                }
+            }
+        }
+    }
+    return pdir;
+
+    uint64_t *pml4_cv = mmu_native_build_dir();          /* copy,     virtual */
+    uint64_t *pml4_ov = native_p_to_v(native_get_cr3()); /* original, virtual */
+    uint64_t *pdpt_ov = NULL;
+    uint64_t *pd_ov   = NULL;
+    uint64_t *pt_ov   = NULL;
+
+    for (size_t pml4i = 0; pml4i < KPML4I; ++pml4i) {
+        if (pml4_ov[pml4i] & MM_PRESENT) {
+
+
+            for (size_t pdpti = 0; pdpti < 512; ++pdpti)  {
+
+
+            }
+        }
+    }
+
+    return NULL;
+#endif
 }
