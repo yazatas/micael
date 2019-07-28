3486a2ed79c49e99c86071cf16bc01568b603e24
rficu <rficu@airmail.cc>
2019-07-28 09:06:08 +0300

    arch/x86_64: Initialize GDT and IDT only once
    Only BSP needs to initialize these tables and reinitializing them
    while they're in use might cause some problems
    
    While there, fix some indentation issues in idt.c

--- kernel/arch/x86_64/gdt.c
+++ kernel/arch/x86_64/gdt.c
@@ -1,6 +1,7 @@
 #include <kernel/gdt.h>
 #include <kernel/kprint.h>
 #include <kernel/util.h>
+#include <stdbool.h>
 
 extern unsigned long boot_stack;
 
@@ -10,14 +11,19 @@ struct tss_ptr_t tss_ptr;
 
 void gdt_init(void)
 {
-    GDT[0] = 0UL;
-    GDT[1] = 0x00a0980000000000UL; /* kernel code */
-    GDT[2] = 0x00c0920000000000UL; /* kernel data */
-    GDT[3] = 0x00a0f80000000000UL; /* user code */
-    GDT[4] = 0x00c0f20000000000UL; /* user data */
+    static bool initialized = false;
 
-    gdt_ptr.limit = sizeof(GDT) - 1;
-    gdt_ptr.base  = (uint64_t)GDT;
+    if (initialized == false) {
+        GDT[0] = 0UL;
+        GDT[1] = 0x00a0980000000000UL; /* kernel code */
+        GDT[2] = 0x00c0920000000000UL; /* kernel data */
+        GDT[3] = 0x00a0f80000000000UL; /* user code */
+        GDT[4] = 0x00c0f20000000000UL; /* user data */
+
+        gdt_ptr.limit = sizeof(GDT) - 1;
+        gdt_ptr.base  = (uint64_t)GDT;
+        initialized   = true;
+    }
 
     gdt_flush(&gdt_ptr);
 }
--- kernel/arch/x86_64/idt.c
+++ kernel/arch/x86_64/idt.c
@@ -2,6 +2,7 @@
 #include <kernel/idt.h>
 #include <kernel/kprint.h>
 #include <kernel/util.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 /* defined in arch/x86_64/interrupts.s */
@@ -35,45 +36,50 @@ struct idt_entry_t *IDT = idt_table;
 
 void idt_set_gate(unsigned long offset, uint16_t select, uint8_t type, struct idt_entry_t *entry)
 {
-	entry->offset0_15  = offset & 0xffff;
-	entry->offset16_31 = (offset >> 16) & 0xffff;
+    entry->offset0_15  = offset & 0xffff;
+    entry->offset16_31 = (offset >> 16) & 0xffff;
     entry->offset32_63 = (offset >> 32) & 0xffffffff;
-	entry->select      = select;
+    entry->select      = select;
     entry->ist         = 0;
-	entry->type        = type;
+    entry->type        = type;
     entry->reserved1   = 0;
     entry->reserved2   = 0;
 }
 
 void idt_init(void)
 {
-	idt_ptr.limit = IDT_ENTRY_SIZE * 256 - 1;
-	idt_ptr.base  = (unsigned long)idt_table;
+    static bool initialized = false;
 
-	kmemset(idt_table, 0, IDT_ENTRY_SIZE * IDT_TABLE_SIZE);
-    
-	idt_set_gate((unsigned long)isr0,   0x08, 0x8e, &idt_table[0]);
-	idt_set_gate((unsigned long)isr1,   0x08, 0x8e, &idt_table[1]);
-	idt_set_gate((unsigned long)isr2,   0x08, 0x8e, &idt_table[2]);
-	idt_set_gate((unsigned long)isr3,   0x08, 0x8e, &idt_table[3]);
-	idt_set_gate((unsigned long)isr4,   0x08, 0x8e, &idt_table[4]);
-	idt_set_gate((unsigned long)isr5,   0x08, 0x8e, &idt_table[5]);
-	idt_set_gate((unsigned long)isr6,   0x08, 0x8e, &idt_table[6]);
-	idt_set_gate((unsigned long)isr7,   0x08, 0x8e, &idt_table[7]);
-	idt_set_gate((unsigned long)isr8,   0x08, 0x8e, &idt_table[8]);
-	idt_set_gate((unsigned long)isr9,   0x08, 0x8e, &idt_table[9]);
-	idt_set_gate((unsigned long)isr10,  0x08, 0x8e, &idt_table[10]);
-	idt_set_gate((unsigned long)isr11,  0x08, 0x8e, &idt_table[11]);
-	idt_set_gate((unsigned long)isr12,  0x08, 0x8e, &idt_table[12]);
-	idt_set_gate((unsigned long)isr13,  0x08, 0x8e, &idt_table[13]);
-	idt_set_gate((unsigned long)isr14,  0x08, 0x8e, &idt_table[14]);
-	idt_set_gate((unsigned long)isr15,  0x08, 0x8e, &idt_table[15]);
-	idt_set_gate((unsigned long)isr16,  0x08, 0x8e, &idt_table[16]);
-	idt_set_gate((unsigned long)isr17,  0x08, 0x8e, &idt_table[17]);
-	idt_set_gate((unsigned long)isr18,  0x08, 0x8e, &idt_table[18]);
-	idt_set_gate((unsigned long)isr19,  0x08, 0x8e, &idt_table[19]);
-	idt_set_gate((unsigned long)isr20,  0x08, 0x8e, &idt_table[20]);
-	idt_set_gate((unsigned long)isr128, 0x08, 0xee, &idt_table[128]);
+    if (initialized == false) {
+        kmemset(idt_table, 0, IDT_ENTRY_SIZE * IDT_TABLE_SIZE);
+
+        idt_set_gate((unsigned long)isr0,   0x08, 0x8e, &idt_table[0]);
+        idt_set_gate((unsigned long)isr1,   0x08, 0x8e, &idt_table[1]);
+        idt_set_gate((unsigned long)isr2,   0x08, 0x8e, &idt_table[2]);
+        idt_set_gate((unsigned long)isr3,   0x08, 0x8e, &idt_table[3]);
+        idt_set_gate((unsigned long)isr4,   0x08, 0x8e, &idt_table[4]);
+        idt_set_gate((unsigned long)isr5,   0x08, 0x8e, &idt_table[5]);
+        idt_set_gate((unsigned long)isr6,   0x08, 0x8e, &idt_table[6]);
+        idt_set_gate((unsigned long)isr7,   0x08, 0x8e, &idt_table[7]);
+        idt_set_gate((unsigned long)isr8,   0x08, 0x8e, &idt_table[8]);
+        idt_set_gate((unsigned long)isr9,   0x08, 0x8e, &idt_table[9]);
+        idt_set_gate((unsigned long)isr10,  0x08, 0x8e, &idt_table[10]);
+        idt_set_gate((unsigned long)isr11,  0x08, 0x8e, &idt_table[11]);
+        idt_set_gate((unsigned long)isr12,  0x08, 0x8e, &idt_table[12]);
+        idt_set_gate((unsigned long)isr13,  0x08, 0x8e, &idt_table[13]);
+        idt_set_gate((unsigned long)isr14,  0x08, 0x8e, &idt_table[14]);
+        idt_set_gate((unsigned long)isr15,  0x08, 0x8e, &idt_table[15]);
+        idt_set_gate((unsigned long)isr16,  0x08, 0x8e, &idt_table[16]);
+        idt_set_gate((unsigned long)isr17,  0x08, 0x8e, &idt_table[17]);
+        idt_set_gate((unsigned long)isr18,  0x08, 0x8e, &idt_table[18]);
+        idt_set_gate((unsigned long)isr19,  0x08, 0x8e, &idt_table[19]);
+        idt_set_gate((unsigned long)isr20,  0x08, 0x8e, &idt_table[20]);
+        idt_set_gate((unsigned long)isr128, 0x08, 0xee, &idt_table[128]);
+
+        idt_ptr.limit = IDT_ENTRY_SIZE * 256 - 1;
+        idt_ptr.base  = (unsigned long)idt_table;
+        initialized   = true;
+    }
 
     idt_flush(&idt_ptr);
 }
