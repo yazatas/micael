7a013be704ea39cd9a539cebe9d1f7ee896a01d0
rficu <rficu@airmail.cc>
2019-07-31 19:33:53 +0300

    arch/x86_64: Add percpu variables
    Each CPU has its own copy of the variables and this is an essential
    feature for f.ex. multiprocessor scheduling.
    
    Currently only the get_* macros are useful but kernel preemption is
    a planned feature so it is very much encouraged to pair every get_*
    with put_*.
    
    doc/SMP.md contains more information about percpu variables

--- kernel/include/kernel/compiler.h
+++ kernel/include/kernel/compiler.h
@@ -4,6 +4,6 @@
 #define __packed   __attribute__((packed))
 #define __align_4k __attribute__((aligned(4096)))
 #define __noreturn __attribute__((noreturn))
-#define __percpu   __attribute__((noreturn))
+#define __percpu   __attribute__((section(".percpu")))
 
 #endif /* end of include guard: __COMPILER_H__ */
