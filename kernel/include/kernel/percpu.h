1dea76a918af84ad0cfd551cf57240405dd74c72
rficu <rficu@airmail.cc>
2019-08-02 19:09:48 +0300

    kernel: Fix percpu size calculation
    The calculation must be done explicitly for every operation because
    AFAIK there's no way to save the size to header file.

--- kernel/include/kernel/percpu.h
+++ kernel/include/kernel/percpu.h
@@ -3,23 +3,18 @@
 
 #include <kernel/cpu.h>
 
-static unsigned long __percpu_size = 0xdeadbeef;
+extern uint8_t _percpu_start, _percpu_end;
 
-static inline void percpu_init(int cpu)
-{
-    extern uint8_t _percpu_start, _percpu_end;
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
 
-    if (__percpu_size == 0xdeadbeef)
-        __percpu_size = (uint64_t)&_percpu_end - (uint64_t)&_percpu_start;
-
-    set_msr(GS_BASE, cpu * __percpu_size);
-}
-
+#define __percpu_size           ((uint64_t)((uint64_t)&_percpu_end - (uint64_t)&_percpu_start))
 #define __this_cpu_ptr(var)     ((typeof(var) *)(((uint8_t *)(&(var))) + get_msr(GS_BASE)))
 #define __any_cpu_ptr(var, cpu) ((typeof(var) *)(((uint8_t *)(&(var))) + cpu * __percpu_size))
 
 #define get_thiscpu_var(var) *(__this_cpu_ptr(var))
 #define get_thiscpu_ptr(var)  (__this_cpu_ptr(var))
+#define get_thiscpu_id()      (get_msr(GS_BASE) / __percpu_size)
 
 #define put_thiscpu_var(var) ((void)(var))
 #define put_thiscpu_ptr(var) ((void)(var))
@@ -29,5 +24,9 @@ static inline void percpu_init(int cpu)
 
 #define put_percpu_var(var, cpu) ((void)(&(var)))
 #define put_percpu_ptr(var, cpu) ((void)(var))
+
+#define percpu_init(cpu)         (set_msr(GS_BASE, (cpu) * __percpu_size))
+
+#pragma GCC pop_options
 
 #endif /* __PERCPU_H__ */
