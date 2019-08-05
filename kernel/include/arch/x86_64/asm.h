9f29dfdf6314cfdf776c6757d7363a8d0d46c831
rficu <rficu@airmail.cc>
2019-08-05 18:02:18 +0300

    arch/x86_64: Move the definition of context switch to asm.h

--- kernel/include/arch/x86_64/asm.h
+++ kernel/include/arch/x86_64/asm.h
@@ -1,8 +1,11 @@
 #ifndef __X86_64_ASM_H__
 #define __X86_64_ASM_H__
 
+#include <kernel/compiler.h>
+
 void gdt_flush(void *ptr);
 void idt_flush(void *ptr);
 void tss_flush(int index);
+void __noreturn context_switch(unsigned long, void *);
 
 #endif /* __X86_64_ASM_H____X86_64_ASM_H__ */
