fec31718424d4f9222b641814377cc6cea2fbd12
rficu <rficu@airmail.cc>
2021-04-18 15:50:51 +0300

    net: Implement ipv4_send_datagram()

--- kernel/include/net/ipv4.h
+++ kernel/include/net/ipv4.h
@@ -3,14 +3,27 @@
 
 #include <kernel/compiler.h>
 #include <kernel/common.h>
+#include <net/util.h>
 
 typedef struct ipv4_datagram {
-    uint8_t dst[6];
-    uint8_t src[6];
-    uint16_t type;
-    char payload[0];
+    uint8_t version:4;
+    uint8_t ihl:4;
+    uint8_t dscp:6;
+    uint8_t ecn:2;
+    uint16_t len;
+    uint16_t iden;
+    uint8_t flags:3;
+    uint16_t frag_off:13;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t checksum;
+    uint32_t src;
+    uint32_t dst;
+    char payload[0]; /* may contain IHL */
 } __packed ipv4_datagram_t;
 
 int ipv4_handle_datagram(ipv4_datagram_t *dgram, size_t size);
+
+int ipv4_send_datagram(ip_t *src, ip_t *dst, void *payload, size_t size);
 
 #endif /* __IPV4_H__ */
--- kernel/net/ipv4.c
+++ kernel/net/ipv4.c
@@ -1,6 +1,64 @@
+#include <errno.h>
+#include <kernel/kassert.h>
+#include <kernel/kpanic.h>
+#include <kernel/util.h>
+#include <mm/heap.h>
+#include <net/arp.h>
+#include <net/eth.h>
 #include <net/ipv4.h>
+#include <net/udp.h>
+
+enum {
+    PROTO_UDP = 0x11,
+};
+
+uint8_t E2TH_BROADCAST[6] = {
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+};
+
+static ip_t IPV4_BROADCAST = {
+    .addr = "255.255.255.255",
+    .ipv4 = { 255, 255, 255, 255 }
+};
 
 int ipv4_handle_datagram(ipv4_datagram_t *dgram, size_t size)
 {
     return 0;
+}
+
+int ipv4_send_datagram(ip_t *src, ip_t *dst, void *payload, size_t size)
+{
+    kassert(dst && payload && size)
+
+    if (!dst || !payload || !size) {
+        kprint("ipv4 - invalid dst addr (0x%x), payload (0x%x) or size (%u)\n",
+                dst, payload, size);
+        return -EINVAL;
+    }
+
+    int ret;
+    ipv4_datagram_t *dgram = kzalloc(sizeof(ipv4_datagram_t) + size);
+    uint8_t hw_addr[6]     = { 0 };
+
+    if (!kstrcmp(dst->addr, IPV4_BROADCAST.addr)) {
+        kmemset(hw_addr, 0xff, sizeof(hw_addr));
+    } else {
+        kpanic("ipv4 - resolve address, todo");
+    }
+
+    dgram->version  = 5;
+    dgram->ihl      = 4;
+    dgram->len      = h2n_16(sizeof(ipv4_datagram_t) + size);
+    dgram->ttl      = 128;
+    dgram->protocol = PROTO_UDP;
+    dgram->checksum = 0; /* TODO:  */
+
+    kmemcpy(&dgram->src, src->ipv4, sizeof(dgram->src));
+    kmemcpy(&dgram->dst, dst->ipv4, sizeof(dgram->dst));
+    kmemcpy(&dgram->payload, payload, size);
+
+    ret = eth_send_frame(hw_addr, ETH_TYPE_IPV4, dgram, sizeof(ipv4_datagram_t) + size);
+
+    kfree(dgram);
+    return ret;
 }
