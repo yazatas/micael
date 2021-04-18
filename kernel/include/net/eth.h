97ff516eb05deece63d69e1f929646879c985a5a
rficu <rficu@airmail.cc>
2021-04-18 11:29:13 +0300

    net: Implement eth_send_frame()
    For now the stack is the opposite of zero-copy as each layer will
    append the headers before the payload by making a copy but in the
    near future I'll implement a packet allocator that reserves
    space for the lower-level headers.

--- kernel/include/net/eth.h
+++ kernel/include/net/eth.h
@@ -4,6 +4,12 @@
 #include <kernel/compiler.h>
 #include <kernel/common.h>
 
+enum {
+    ETH_TYPE_IPV4 = 0x0800,
+    ETH_TYPE_ARP  = 0x0806,
+    ETH_TYPE_IPV6 = 0x86dd,
+};
+
 typedef struct eth_frame {
     uint8_t dst[6];
     uint8_t src[6];
@@ -12,5 +18,6 @@ typedef struct eth_frame {
 } __packed eth_frame_t;
 
 int eth_handle_frame(eth_frame_t *frame, size_t size);
+int eth_send_frame(uint8_t dst[6], uint16_t type, uint8_t *payload, size_t size);
 
 #endif /* __ETH_H__ */
--- kernel/net/eth.c
+++ kernel/net/eth.c
@@ -1,16 +1,15 @@
 #include <errno.h>
+#include <drivers/net/rtl8139.h>
+#include <kernel/kassert.h>
+#include <kernel/util.h>
+#include <mm/heap.h>
+#include <net/arp.h>
 #include <net/eth.h>
 #include <net/ipv4.h>
 #include <net/ipv6.h>
 #include <net/util.h>
 
 #define ETH_HDR_SIZE 14
-
-enum {
-    ETH_TYPE_IPV4 = 0x0800,
-    ETH_TYPE_ARP  = 0x0806,
-    ETH_TYPE_IPV6 = 0x86dd,
-};
 
 int eth_handle_frame(eth_frame_t *frame, size_t size)
 {
@@ -24,4 +23,26 @@ int eth_handle_frame(eth_frame_t *frame, size_t size)
     }
 
     return 0;
+}
+
+int eth_send_frame(uint8_t dst[6], uint16_t type, uint8_t *payload, size_t size)
+{
+    kassert(payload && size);
+
+    if (!payload || !size) {
+        kprint("eth - payload (0x%x) or size (0x%x) is invalid!");
+        return -EINVAL;
+    }
+
+    eth_frame_t *eth = kzalloc(sizeof(eth_frame_t) + size);
+
+    kmemcpy(eth->dst, dst, sizeof(eth->dst));
+    kmemcpy(eth->src, &(uint64_t) { rtl8139_get_mac() }, sizeof(eth->src));
+    kmemcpy(eth->payload, payload, size);
+
+    eth->type = h2n_16(ETH_TYPE_ARP);
+
+    rtl8139_send_pkt((uint8_t *)eth, sizeof(eth_frame_t) + size);
+
+    kfree(eth);
 }
