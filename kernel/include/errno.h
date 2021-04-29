e15237611f94918641f5eaf5ca11286cc2639abb
rficu <rficu@airmail.cc>
2021-04-29 13:46:43 +0300

    kernel: Introduce EFAULT

--- kernel/include/errno.h
+++ kernel/include/errno.h
@@ -16,6 +16,7 @@ int errno;
 #define ENOTSUP 11      /* Operation not supported */
 #define ENXIO   12      /* No such device or address */
 #define EAGAIN  13      /* Try again */
+#define EFAULT  14      /* Bad address */
 #define EINPROGRESS 115 /* Operation now in progress */
 
 #define EMAX    13 /* Used by the kstrerror() */
