c048ccb8d8172b9d50e72d4ef63a5cb66562176e
rficu <rficu@airmail.cc>
2021-05-03 05:34:10 +0300

    net/udp: Implement UDP socket buffer routines

--- kernel/include/net/udp.h
+++ kernel/include/net/udp.h
@@ -1,15 +1,49 @@
 #ifndef __UDP_H__
 #define __UDP_H__
 
+#include <fs/fs.h>
 #include <kernel/compiler.h>
 #include <kernel/common.h>
 #include <net/util.h>
 #include <net/types.h>
 
+#define SKB_MAX_SIZE 10
+
+typedef struct socket socket_t;
+
+typedef struct udp_skb {
+    spinlock_t lock;
+    size_t rptr;
+    size_t wptr;
+    size_t npkts;
+    packet_t *packets[SKB_MAX_SIZE];
+} udp_skb_t;
+
 /* TODO:  */
 int udp_handle_pkt(packet_t *pkt);
 
-
 int udp_send_pkt(packet_t *pkt);
+
+/* TODO:  */
+void udp_init_socket(file_t *fd);
+
+/* Read a datagram from the UDP socket buffer if there are any available
+ *
+ * If "size" is smaller than the oldest available datagram, the
+ * packet is truncated to fit into "buf".
+ *
+ * Return the number of bytes copied on success
+ * Return 0 if the socket buffer is empty
+ * Return -EINVAL if one of the parameters is invalid */
+int udp_read_skb(socket_t *sock, void *buf, size_t size);
+
+/* Write a packet to the UDP socket buffer
+ *
+ * If the buffer is full, the oldest packet is
+ * overwritten with the new packet
+ *
+ * Return 0 on success
+ * Return -EINVAL if one of the parameters is invalid */
+int udp_write_skb(socket_t *sock, packet_t *pkt);
 
 #endif /* __UDP_H__ */
--- kernel/net/udp.c
+++ kernel/net/udp.c
@@ -5,9 +5,55 @@
 #include <net/dhcp.h>
 #include <net/ipv4.h>
 #include <net/ipv6.h>
+#include <net/netdev.h>
+#include <net/socket.h>
 #include <net/udp.h>
+#include <sched/sched.h>
+#include <sync/wait.h>
+#include <sys/socket.h>
 
-#define UDP_HDR_SIZE 8
+#define UDP_PAYLOAD_MAX 65507
+
+static ssize_t __udp_write(file_t *file, off_t flags, size_t size, void *buf)
+{
+    if (size > UDP_PAYLOAD_MAX)
+        return -EINVAL;
+
+    socket_t *sock = file->f_private;
+    packet_t *pkt  = buf;
+
+    return udp_send_pkt(pkt);
+}
+
+static ssize_t __udp_read(file_t *file, off_t flags, size_t size, void *buf)
+{
+    int ret;
+    socket_t *sock = file->f_private;
+
+    if (!sock->udp->npkts) {
+        if (flags & MSG_DONTWAIT)
+            return -EAGAIN;
+
+        task_t *current = sched_get_active();
+        wq_wait_event(&sock->wq, current, NULL);
+    }
+
+    if ((ret = udp_read_skb(sock, buf, size)) < 0) {
+        kprint("udp - failed to read a datagram from the socket buffer\n");
+        return ret;
+    }
+
+    return ret;
+}
+
+void udp_init_socket(file_t *fd)
+{
+    fd->f_ops->read  = __udp_read;
+    fd->f_ops->write = __udp_write;
+
+    socket_t *sock  = fd->f_private;
+    sock->s_private = NULL;
+}
 
 int udp_handle_pkt(packet_t *pkt)
 {
@@ -41,4 +87,51 @@ int udp_send_pkt(packet_t *pkt)
         return ipv4_send_pkt(pkt);
     else
         return ipv6_send_pkt(pkt);
+}
+
+int udp_read_skb(socket_t *sock, void *buf, size_t size)
+{
+    udp_skb_t *skb = sock->udp;
+
+    if (!skb || !buf || !size)
+        return -EINVAL;
+
+    if (!skb->npkts)
+        return 0;
+
+    spin_acquire(&skb->lock);
+
+    packet_t *pkt   = skb->packets[skb->rptr];
+    size_t cpy_size = MIN(size, ((size_t)pkt->app.size));
+
+    kmemcpy(buf, pkt->app.packet, cpy_size);
+    netdev_dealloc_pkt(pkt);
+    skb->packets[skb->rptr] = NULL;
+
+    skb->npkts--;
+    skb->rptr = (skb->rptr + 1) % SKB_MAX_SIZE;
+
+    spin_release(&skb->lock);
+    return cpy_size;
+}
+
+int udp_write_skb(socket_t *sock, packet_t *pkt)
+{
+    udp_skb_t *skb = sock->udp;
+
+    if (!skb || !pkt)
+        return -EINVAL;
+
+    spin_acquire(&skb->lock);
+
+    if (skb->packets[skb->wptr])
+        netdev_dealloc_pkt(skb->packets[skb->wptr]);
+
+    skb->packets[skb->wptr] = pkt;
+    skb->wptr = (skb->wptr + 1) % SKB_MAX_SIZE;
+    skb->npkts++;
+
+    spin_release(&skb->lock);
+
+    return 0;
 }
